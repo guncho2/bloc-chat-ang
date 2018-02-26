68d67959b26133e84f0434b70d1264bdee8717a9
Eduardo Lowensohn <gunchosoft@gmail.com>
2018-02-26 12:47:18 -0500

    commit 2

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+// Invoke angular and connect it to your application's name with the module method.
+//Include ui.router and firebase as dependencies.
+(function() {
+//Add a configuration function and pass it to the angular config method and
+//inject the $stateProvider and $locationProvider. Use the providers to define
+//your first state which will hold the main view for the application.
+  function config($locationProvider, $stateProvider) {
+         $locationProvider
+             .html5Mode({
+                 enabled: true,
+                 requireBase: false
+              });
+
+         $stateProvider
+             .state('home', {
+                 url: '/',
+                 controller: 'HomeCtrl as home',
+                 templateUrl: '/templates/home.html'
+             });
+     }
+
+     angular
+         .module('blocChatAng', ['ui.router', 'firebase'])
+         .config(config);
+
+ })();
