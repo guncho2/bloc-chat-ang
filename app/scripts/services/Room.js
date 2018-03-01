39c4c3485e114ed3b3fcac9fcc3cae60012626b6
Eduardo Lowensohn <gunchosoft@gmail.com>
2018-03-01 09:21:38 -0500

    commit 5

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,11 +1,11 @@
 (function() {
-  function Room($firebaseArray) {
+  function Room($scope, $firebaseArray) {
     var Room = {};
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
 
     Room.all = rooms;
-
+$scope.rooms = Room.all;
     return Room;
   }
 
